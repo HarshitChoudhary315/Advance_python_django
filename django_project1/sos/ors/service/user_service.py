cad9d2364974d6e65614d6bedde8181d31e6706d
Harsh0305-gif <hc888427@gmai.com>
2026-09-03 12:43:49 +0530

    done

---
+++ django_project1/sos/ors/service/user_service.py
@@ -0,0 +1,35 @@
+from django.contrib.auth import login
+from django.db import connection
+
+
+class UserService:
+
+    def next_pk(self):
+        pk = 0
+        cursor = connection.cursor()
+        sql = "select max(id) from sos_user"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        for data in result:
+            if data[0] is not None:
+                pk = data[0]
+        connection.commit()
+        connection.close()
+        return pk + 1
+
+    def add(self, data):
+        id = UserService.next_pk(self)
+        first_name = data['first_name']
+        last_name = data['last_name']
+        login_id = data['login_id']
+        password = data['password']
+        dob = data['dob']
+        address = data['address']
+
+        cursor = connection.cursor()
+        sql = "insert into sos_user values(%s, %s, %s, %s, %s, %s, %s)"
+        data = (id, first_name, last_name, login_id, password, dob, address)
+        cursor.execute(sql, data)
+        connection.commit()
+        connection.close()
+        print("data inserted successfully")
--- django_project1/sos/ors/views.py
+++ django_project1/sos/ors/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .service.user_service import UserService
+
 
 def test_ors(request):
     return HttpResponse('<h1> this is ors app</h1>')
@@ -9,7 +11,20 @@ def welcome(request):
     return render(request,'welcome.html')
 
 def user_signup(request):
+    if request.method == "POST":
+     form = {}
+     form['first_name'] = request.POST.get('firstName')
+     form['last_name'] = request.POST.get('lastName')
+     form['login_id'] = request.POST.get('loginId')
+     form['password'] = request.POST.get('password')
+     form['dob'] = request.POST.get('dob')
+     form['address'] = request.POST.get('address')
+
+     service = UserService()
+     service.add(form)
     return render(request,'registration.html')
 
 def user_sigin(request):
+    print(request.POST.get('loginId'))
+    print(request.POST.get('password'))
     return render(request,'login.html')
