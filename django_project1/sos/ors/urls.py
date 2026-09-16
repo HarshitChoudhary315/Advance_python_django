46ebe66abf99f9bfc7dc0f96ec10000883157ae1
Harsh0305-gif <hc888427@gmai.com>
2026-09-16 18:42:22 +0530

    done

--- django_project1/sos/ors/urls.py
+++ django_project1/sos/ors/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('testors/',views.test_ors),
     path('welcome/',views.welcome),
     path('signup/',views.user_signup),
-    path('signin/',views.user_sigin),
+    path('signin/',views.user_signin),
     path('logout/',views.user_logout),
     path('',views.welcome),
     path('testlist/',views.test_list),
--- django_project1/sos/ors/views.py
+++ django_project1/sos/ors/views.py
@@ -5,63 +5,82 @@ from .service.user_service import UserService
 
 
 def test_ors(request):
-    return HttpResponse('<h1> this is ors app</h1>')
+    return HttpResponse('<h1>test ors app</h1>')
+
 
 def welcome(request):
-    return render(request,'welcome.html')
+    return render(request, 'welcome.html')
+
 
 def user_signup(request):
     if request.method == "POST":
-     form = {}
-     form['first_name'] = request.POST.get('firstName')
-     form['last_name'] = request.POST.get('lastName')
-     form['login_id'] = request.POST.get('loginId')
-     form['password'] = request.POST.get('password')
-     form['dob'] = request.POST.get('dob')
-     form['address'] = request.POST.get('address')
+        form = {}
+        form['first_name'] = request.POST.get('firstName')
+        form['last_name'] = request.POST.get('lastName')
+        form['login_id'] = request.POST.get('loginId')
+        form['password'] = request.POST.get('password')
+        form['dob'] = request.POST.get('dob')
+        form['address'] = request.POST.get('address')
 
-     service = UserService()
-     service.add(form)
-    return render(request,'registration.html')
+        service = UserService()
+        service.add(form)
 
-def user_sigin(request):
+    return render(request, 'registration.html')
+
+
+def user_signin(request):
     message = ''
-    if request.method =="POST":
+    if request.method == "POST":
         form = {}
         form['login_id'] = request.POST.get('loginId')
         form['password'] = request.POST.get('password')
+
         service = UserService()
-        records = service.authenticate( form['login_id'],form['password'])
+        user_data = service.authenticate(form['login_id'], form['password'])
 
-        if len(records) > 0:
-            request.session['first_name'] = records[0].get('first_name')
-            return render(request,'welcome.html',{'firstName': records[0].get('first_name')})
+        if len(user_data) > 0:
+            request.session['first_name'] = user_data[0].get('first_name')
+            return redirect('/ors/welcome/')
         else:
-            message = 'login & password Invalid'
+            message = 'login & password invalid'
 
-    return render(request,'login.html',{'message':message})
+    return render(request, 'login.html', {'message': message})
+
 
 def user_logout(request):
     request.session['first_name'] = None
     return redirect('/ors/signin/')
 
 
-
 def test_list(request):
     list = [
-        {"id":1,"first_name":"Rahul","last_name":"Sharma","email":"rahul@gmail.com","password":"rahul123"},
-        {"id":2,"first_name": "Pryia","last_name": "Verma","email":"pryia@gmail.com","password":"pryia123"},
-        {"id":3,"first_name": "Amit","last_name": "Patel","email":"amit@gmail.com","password":"amit123"},
-        {"id":4,"first_name": "Neha","last_name": "Singh","email":"neha@gmail.com","password":"neha123"},
-        {"id":5,"first_name": "Rohit","last_name": "Gupta","email":"rohit@gmail.com","password":"rohit123"}
+        {"id": 1, "first_name": "Rahul", "last_name": "Sharma", "email": "rahul@gmail.com", "password": "rahul123"},
+        {"id": 2, "first_name": "Priya", "last_name": "Verma", "email": "priya@gmail.com", "password": "priya123"},
+        {"id": 3, "first_name": "Amit", "last_name": "Patel", "email": "amit@gmail.com", "password": "amit123"},
+        {"id": 4, "first_name": "Neha", "last_name": "Singh", "email": "neha@gmail.com", "password": "neha123"},
+        {"id": 5, "first_name": "Rohit", "last_name": "Gupta", "email": "rohit@gmail.com", "password": "rohit123"}
     ]
-    return render(request,'test_list.html',{'list':list})
+    return render(request, "test_list.html", {"list": list})
+
 
 def user_list(request):
     form = {}
     form['page_no'] = 1
     form['page_size'] = 5
 
+    if request.method == "POST":
+        if request.POST['operation'] == "next":
+            form['page_no'] = int(request.POST.get('pageNo'))
+            form['page_no'] += 1
+
+        if request.POST['operation'] == "previous":
+            form['page_no'] = int(request.POST.get('pageNo'))
+            form['page_no'] -= 1
+
+        if request.POST['operation'] == "search":
+            form['page_no'] = 1
+            form['first_name'] = request.POST.get('firstName')
+
     service = UserService()
     list = service.search(form)
-    return render(request,'user_list.html',{'list':list})
+    return render(request, "user_list.html", {"list": list, 'page_no': form['page_no']})
